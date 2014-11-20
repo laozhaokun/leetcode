05b194a15a9f71cbeba4aec49f9bc535f4ee5bc9
laozhaokun <zhf.thu@gmail.com>
2014-11-20 20:17:52 +0800

    add

---
+++ java/UniquePathsII.java
@@ -0,0 +1,28 @@
+package leetcode;
+
+/**
+ * @author:zhaohf@asiainfo.com
+ * @date:2014年11月20日 下午1:49:07
+ * @Description: TODO
+ */
+public class UniquePathsII {
+	public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+		int m = obstacleGrid.length, n = obstacleGrid[0].length;
+		int[][] dp = new int[m][n];
+		for (int i = 0; i < m; i++) {
+			for (int j = 0; j < n; j++) {
+				if (obstacleGrid[i][j] == 0) {
+					if (i == 0 && j == 0)
+						dp[i][j] = 1;
+					else if (i == 0)
+						dp[i][j] = dp[i][j - 1];
+					else if (j == 0)
+						dp[i][j] = dp[i - 1][j];
+					else
+						dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+				}
+			}
+		}
+		return dp[m - 1][n - 1];
+	}
+}
